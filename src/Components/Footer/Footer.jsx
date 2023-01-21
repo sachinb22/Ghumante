74c0e484c518e0521a1d8cb2749b194db944a6da
AsterUX2 <102274869+AsterUX2@users.noreply.github.com>
2023-01-21 17:23:07 +0545

    Landing Page completed

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect } from "react";
+import "./footer.css";
+import video from "../../Assets/video.mp4";
+import { FiChevronRight, FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { FaFacebookF, FaYoutube } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const Footer = () => {
+  //lets create a react hook to add a scroll animation..
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <>
+      <section className="footer">
+        <div className="videoDiv">
+          <video src={video} muted autoPlay loop type="video/mp4"></video>
+        </div>
+        <div className="secContent container">
+          <div className="contactDiv flex">
+            <div className="text">
+              <small data-aos="fade-up">KEEP IN TOUCH</small>
+              <h2 data-aos="fade-up">Travel with us</h2>
+            </div>
+            <div className="inputDiv flex">
+              <input
+                data-aos="fade-up"
+                type="text"
+                placeholder="Enter Email Address"
+              />
+              <button data-aos="fade-up" className="btn flex" type="submit">
+                SEND <FiSend className="icon" />
+              </button>
+            </div>
+          </div>
+          <div data-aos="fade-up" className="footerCard flex">
+            <div className="footerIntro flex">
+              <div className="logoDiv">
+                <a href="" className="logo flex">
+                  <MdOutlineTravelExplore className="icon" /> Ghumnate.
+                </a>
+              </div>
+              <div data-aos="fade-up" className="footerParagraph">
+                Ghumante showcases the unique beauty and diversity of the
+                country, Nepal and provide visitors with the information they
+                need to have a safe and enjoyable trip.
+              </div>
+              <div data-aos="fade-up" className="footerSocials flex">
+                <FaFacebookF className="icon" />
+                <AiFillInstagram className="icon" />
+                <FaYoutube className="icon" />
+              </div>
+            </div>
+
+            <div className="footerLinks grid">
+              <div
+                data-aos="fade-up"
+                data-aos-duration="3000"
+                className="linkGroup"
+              >
+                <span className="groupTitle">OUR AGENCY</span>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Services
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Agency
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Tourism
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Payment
+                </li>
+              </div>
+              <div
+                data-aos="fade-up"
+                data-aos-duration="4000"
+                className="linkGroup"
+              >
+                <span className="groupTitle">OUR AGENCY</span>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Services
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Agency
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Tourism
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Payment
+                </li>
+              </div>
+              <div
+                data-aos="fade-up"
+                data-aos-duration="5000"
+                className="linkGroup"
+              >
+                <span className="groupTitle">OUR AGENCY</span>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Services
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Agency
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Tourism
+                </li>
+                <li className="footerList flex">
+                  <FiChevronRight className="icon" /> Payment
+                </li>
+              </div>
+            </div>
+
+            <div className="footerDiv flex">
+              <small>BEST TRAVEL WEBSITE</small>
+              <small>Copyright Ghumante. 2023 All right Reserved. </small>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect } from "react";
+import "./main.css";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BsArrowRight } from "react-icons/bs";
+
+import img from "../../Assets/img1.jpg";
+import img2 from "../../Assets/img2.jpg";
+import img3 from "../../Assets/img3.jpg";
+import img4 from "../../Assets/img4.jpg";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    desTitle: "Swoyambhunath Stupa",
+    location: "Pokhara",
+    fees: "Rs. 2500",
+    description:
+      "Pokhara Grande is the five-star hotel of Pokhara, Nepal providing the right blend of service, luxury, and quiet efficiency.",
+  },
+  {
+    id: 2,
+    imgSrc: img2,
+    desTitle: "Phewa Lake",
+    location: "Pokhara, Nepal",
+    fees: "Rs. 2500",
+    description:
+      "Lumbini Grande is the five-star hotel of Pokhara, Nepal providing the right blend of service, luxury, and quiet efficiency.",
+  },
+  {
+    id: 3,
+    imgSrc: img3,
+    desTitle: "Hotel Lumbini Grande",
+    location: "Gosaikunda, Nepal",
+    fees: "Rs. 2500",
+    description:
+      "Lumbini Grande is the five-star hotel of Pokhara, Nepal providing the right blend of service, luxury, and quiet efficiency.",
+  },
+  {
+    id: 4,
+    imgSrc: img4,
+    desTitle: "Manang",
+    location: "Mustang, Nepal",
+    fees: "Rs. 2500",
+    description:
+      "Lumbini Grande is the five-star hotel of Pokhara, Nepal providing the right blend of service, luxury, and quiet efficiency.",
+  },
+  {
+    id: 5,
+    imgSrc: img,
+    desTitle: "Hotel Lumbini Grande",
+    location: "Pokhara",
+    fees: "Rs. 2500",
+    description:
+      "Lumbini Grande is the five-star hotel of Pokhara, Nepal providing the right blend of service, luxury, and quiet efficiency.",
+  },
+  {
+    id: 6,
+    imgSrc: img,
+    desTitle: "Hotel Lumbini Grande",
+    location: "Pokhara",
+    fees: "Rs. 2500",
+    description:
+      "Lumbini Grande is the five-star hotel of Pokhara, Nepal providing the right blend of service, luxury, and quiet efficiency.",
+  },
+];
+
+const Main = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 data-aos="fade-right" className="title">
+          Most visited destinations
+        </h3>
+      </div>
+      <div className="secContent grid">
+        {Data.map(({ id, imgSrc, desTitle, location, fees, description }) => {
+          return (
+            <div key={id} data-aos="fade-up" className="singleDestination">
+              <div className="imageDiv">
+                <img src={imgSrc} alt={desTitle} />
+              </div>
+              <div className="cardInfo">
+                <h4 className="desTitle">{desTitle}</h4>
+                <span className="continent flex">
+                  <HiOutlineLocationMarker className="icon" />
+                  <span className="name">{location}</span>
+                </span>
+                <div className="fees flex">
+                  <span>per day/-</span>
+                  <div className="price">
+                    <h5>{fees}</h5>
+                  </div>
+                </div>
+                <div className="desc">{description}</div>
+
+                <button className="btn flex">
+                  Details <BsArrowRight className="icon" />
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Main;
